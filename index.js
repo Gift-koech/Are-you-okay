a81f4284d39ba02450dc950e124bbb53bea628ac
Gift-koech <koechgift93@gmail.com>
2022-09-08 15:03:19 +0300

    modifired the searchbar  event listener

--- index.js
+++ index.js
@@ -7,9 +7,7 @@ const options = {
 };
 
 
-search = document.querySelector("#searchbar")
- search.addEventListener("submit",(e)=>{
-    e.preventDefault()
+
     console.log(e.target.search.value);
     fetch(`https://mental-health-info-api.p.rapidapi.com/news/${e.target.search.value}`, options).then(response => {
         return response.json(); 
